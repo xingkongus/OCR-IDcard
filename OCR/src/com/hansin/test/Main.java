de96a04bd06207dd4029a888f5a4e23f116bea7f
Hansin1997 <845612500@qq.com>
2017-12-13 02:49:15 +0800

    init

--- OCR/src/com/hansin/test/Main.java
+++ OCR/src/com/hansin/test/Main.java
@@ -6,12 +6,18 @@ public class Main {
 
 	public static void main(String[] args) throws Throwable {
 
-		String appKey = "wo0YGS7fd8Ogx1Vt2SsUQcSd";//百度API Key
-		String secretKey = "N1lsxFG5PuGLGH1iUthPistToGPojTeo";//SercetKey
+		String appKey = "wo0YGS7fd8Ogx1Vt2SsUQcSd";				//百度API Key
+		String secretKey = "N1lsxFG5PuGLGH1iUthPistToGPojTeo";	//SercetKey
+		
+		String path = "d:/files/idcard/";						//身份证照片文件夹
+		
+		String csvFile = "d:/a.csv";							//导出csv文件保存位置
+		
 		
 		Recoder recoder = new Recoder(appKey,secretKey );
 		
-		float persent = recoder.startRecode("d:/files/idcard/", "d:/a.csv");
+		float persent = recoder.startRecode(path, csvFile);		//开始分析
+		
 		System.out.println("done. " +  persent * 100 + "%");
 	}
 }
